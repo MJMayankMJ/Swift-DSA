f0306d36585cb45871d4e68fc54804fa605009b6
Mayank Jangid <226301131@gkv.ac.in>
2025-03-26 00:48:30 +0530

    initial

--- Sources/Swift DSA/Swift_DSA.swift
+++ Sources/Swift DSA/Swift_DSA.swift
@@ -1,2 +1,148 @@
 // The Swift Programming Language
 // https://docs.swift.org/swift-book
+import Foundation
+
+//MARK: - 1. Two Sum (#1)
+
+//public func twoSum (nums: [Int], target: Int) -> Bool {
+//     var seenSet : Set<Int> = []
+//    for num in nums {
+//        let diff = target - num
+//        if seenSet.contains(diff) {
+//            return true
+//        } else {
+//            seenSet.insert(num)
+//        }
+//    }
+//    return false
+//}
+
+func twoSumm(_ nums: [Int], _ target: Int) -> [Int] {
+       let sortedNums = nums.enumerated().sorted {$0.element < $1.element}
+        var left = 0
+        var right = sortedNums.count - 1
+
+        while left < right {
+            var sum = sortedNums[left].element + sortedNums[right].element
+            if sum == target{
+                 return [sortedNums[left].offset, sortedNums[right].offset]
+            } else if sum < target {
+                left += 1
+            } else {
+                right -= 1
+            }
+        }
+        return []
+    } // nlogn
+
+func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+       var dict = [Int: Int]()
+       for (index, value) in nums.enumerated() {
+            let compliment = target - value
+            if let complimentIndex = dict[compliment]{
+                return [complimentIndex, index]
+            }
+            dict[value] = index
+       }
+       return []
+    } // n
+
+//MARK: - 2. Max Profit (#121)
+
+func maxProfit(_ prices: [Int]) -> Int {
+    var start = 0
+    var end = 1
+    var maxProfit = 0
+    
+    while end <= prices.count - 1 {
+        var startP = prices[start]
+        var endP = prices[end]
+        var profit = endP - startP
+        if startP > endP {
+            start += 1
+        } else {
+            maxProfit = max(maxProfit, profit)
+            end += 1
+        }
+    }
+    return maxProfit
+}
+
+//MARK: - 3. Contains Duplicate (#217)
+
+func containsDuplicate(_ nums: [Int]) -> Bool {
+    let numsSet = Set(nums)
+    if nums.count == numsSet.count {
+        return false
+    }
+    return true
+}
+
+//MARK: - 4. Product of Array Except Self (#238)
+
+func productExceptSelf(_ nums: [Int]) -> [Int] {
+    let n = nums.count
+    var result = Array(repeating: 1, count: n)
+    
+    // prefix stores the multiply of itself and the number before that ( but we save it after wards)
+    // result[i] = prefix -- taki ek step jyada lge --- cz humme usee phle ka store krna hei multiple taki wo khud na aaye
+    var prefix = 1
+    for i in 0..<n {
+        result[i] = prefix
+        prefix *= nums[i]
+    } // result [1,1,2,6]
+    
+    // suffix stores the multiple of itself and the number on the right(cz array is rev) (but we save it after one step so it doesnt include itself)
+    // result[i] *= suffix -- same as above but also multiplies the prev array to itself so to avoid 1 extra loop
+    var suffix = 1
+    for i in (0..<n).reversed() {
+        result[i] *= suffix
+        suffix *= nums[i]
+    }
+    return result
+}
+
+//MARK: - 5. Maximum Subarray (#53)
+
+func maxSubArray(_ nums: [Int]) -> Int {
+    var maxSubArraySum = nums[0]
+    var currentSubArraySum = nums[0]
+    
+    for i in (1..<nums.count){
+        currentSubArraySum = max(nums[i], nums[i] + currentSubArraySum)
+        maxSubArraySum = max(currentSubArraySum, maxSubArraySum)
+    }
+    return maxSubArraySum
+}
+
+//MARK: - 6. Maximum Product Subarray (#152)
+
+    func maxProduct(_ nums: [Int]) -> Int {
+        
+        return 0
+    }
+
+//MARK: - 7. Valid Anagram (#242)
+
+func isAnagramm(_ s: String, _ t: String) -> Bool {
+        if String(s.sorted()) == String(t.sorted()){
+            return true
+        } else {
+            return false
+        }
+    } // nlogn
+
+func isAnagram(_ s: String, _ t: String) -> Bool {
+    guard s.count == t.count else {return false}
+    var charCount = [Character: Int]()
+    for char in s {
+        charCount[char, default : 0] += 1
+    }
+    for char in t {
+        charCount[char, default : 0] -= 1
+        if charCount[char]! < 0 { return false}
+    }
+    return true
+} // n
+
+
--- Tests/Swift DSATests/Swift_DSATests.swift
+++ Tests/Swift DSATests/Swift_DSATests.swift
@@ -2,11 +2,10 @@ import XCTest
 @testable import Swift_DSA
 
 final class Swift_DSATests: XCTestCase {
-    func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
-
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+    func test_twoSum() throws {
+//        let result = twoSum(nums: [2,7,11,15], target: 9)
+//        XCTAssertTrue(result )
+        let result = twoSum([2,7,11,15], 9)
+        XCTAssert(result == [0,1])
     }
 }
